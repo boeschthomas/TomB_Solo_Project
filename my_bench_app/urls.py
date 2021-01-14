d3b96a0557cb71a0f4dfd552ab900172c1c34683
Tom Boesch <tomas@Toms-MacBook-Pro.local>
2021-01-14 09:56:23 -0600

    register changes

---
+++ my_bench_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('register', views.register),
+    path('home', views.home),
+    path('login', views.login),
+    path('my_page/<int:id>', views.my_page),
+    path('comm_page/<int:id>', views.comm_page),
+    path('create_page', views.create_page),
+    path('logout', views.index)	   
+]
---
+++ my_bench_app/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+import bcrypt
+from django.contrib import messages
+from .models import *
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    print(request.POST)
+    errors = User.objects.validator(request.POST)
+    print(errors)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+
+    hashed_password = bcrypt.hashpw(request.POST['pw'].encode(), bcrypt.gensalt()).decode()
+    new_user = User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'], password=hashed_password)
+    print(new_user, "This is my new user!")
+    request.session['fname'] = new_user.first_name
+    request.session['lname'] = new_user.last_name
+    request.session['id'] = new_user.id
+    return redirect('/home')
+
+def login(request):
+    logged_user = User.objects.filter(email=request.POST['email'])
+    if len(logged_user)>0:
+        logged_user = logged_user[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+            print(logged_user, "logged user was signed in!")
+            request.session['fname'] = logged_user.first_name
+            request.session['lname'] = logged_user.last_name
+            request.session['id'] = logged_user.id
+            return redirect('/home')
+
+    return redirect('/')
+
+def home(request):
+    if 'fname' in request.session:
+        return render(request, 'home.html')
+    return redirect('/')
+
+def my_page(request, id):
+    context = {
+        'user': User.objects.all()
+    }
+    return render(request, 'my_page.html', context)
+   
+
+def comm_page(request, id):
+    context = {
+        'all_users': User.objects.all()
+    }
+    return render(request, 'all_users.html', context)
+
+def create_page(request):
+    return render(request, 'my_page.html')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+
+
+
+# Create your views here.
